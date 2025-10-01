30cbb2e2703e24e4807f1eefb9939b6aec0ef2cb
Satwik Singh <satwiksinghrajput2005@gmail.com>
2025-10-01 18:41:31 +0530

    Added

--- musiclibrary.py
+++ musiclibrary.py
@@ -1,4 +1,11 @@
 music = {
-    "starboy": "https://www.youtube.com/watch?v=xS3iRX7_Cxw&list=RDxS3iRX7_Cxw&start_radio=1",
+    "pink lips" : "https://www.youtube.com/watch?v=w5VFOKKAbQQ&list=RDw5VFOKKAbQQ&start_radio=1",
+    "sta rboy": "https://www.youtube.com/watch?v=xS3iRX7_Cxw&list=RDxS3iRX7_Cxw&start_radio=1",
+    "danza kuduro": "https://www.youtube.com/watch?v=XUIXIfm4wQY&list=PLvrbYkA_ZvPsU2ZAp-6nNro3DcWS-2gzp",
     "skyfall": "https://www.youtube.com/watch?v=DeumyOzKqgI&pp=ygUHc2t5ZmFsbA%3D%3D",
+    "espresso": "https://www.youtube.com/watch?v=pSrtKDf5MTs&list=RDpSrtKDf5MTs&start_radio=1",
+    "gangster paradise": "https://www.youtube.com/watch?v=V79D8__4W9g&list=RDV79D8__4W9g&start_radio=1",
+    "mask off": "https://www.youtube.com/watch?v=6Qm7pHtPNmc&list=RD6Qm7pHtPNmc&start_radio=1",
+    "mere mehboob": "https://www.youtube.com/watch?v=UdsO4SM4wKI&list=RDUdsO4SM4wKI&start_radio=1",
+    "solid body" : "https://www.youtube.com/watch?v=ou-litQ9hWQ&list=RDou-litQ9hWQ&start_radio=1"
 }
